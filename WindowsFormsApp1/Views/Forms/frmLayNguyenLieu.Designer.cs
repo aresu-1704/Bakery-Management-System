cb330ee3dbcc652e0a9a80e85570710eb43b30b9
Aresu174 <annalya174@gmail.com>
2025-03-08 22:36:31 +0700

    Reapply "Nhà bếp"
    This reverts commit 3fc235d0a236ae6a570e7d6fc6008c29ef636ad8.

---
+++ WindowsFormsApp1/Views/Forms/frmLayNguyenLieu.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace BakeryManagementSystem.Views.Forms
+{
+    partial class frmLayNguyenLieu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLayNguyenLieu));
+            this.dgvDSNguyenLieu = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.lblMaNL = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTenNL = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.lblDonViTinh = new System.Windows.Forms.Label();
+            this.btnXacNhan = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSNguyenLieu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDSNguyenLieu
+            // 
+            this.dgvDSNguyenLieu.AllowUserToAddRows = false;
+            this.dgvDSNguyenLieu.AllowUserToDeleteRows = false;
+            this.dgvDSNguyenLieu.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDSNguyenLieu.BackgroundColor = System.Drawing.Color.Linen;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Bisque;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.SaddleBrown;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Bisque;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.SaddleBrown;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDSNguyenLieu.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvDSNguyenLieu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvDSNguyenLieu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvDSNguyenLieu.EnableHeadersVisualStyles = false;
+            this.dgvDSNguyenLieu.GridColor = System.Drawing.Color.SaddleBrown;
+            this.dgvDSNguyenLieu.Location = new System.Drawing.Point(12, 119);
+            this.dgvDSNguyenLieu.MultiSelect = false;
+            this.dgvDSNguyenLieu.Name = "dgvDSNguyenLieu";
+            this.dgvDSNguyenLieu.ReadOnly = true;
+            this.dgvDSNguyenLieu.RowHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Linen;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.SaddleBrown;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.PeachPuff;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.SaddleBrown;
+            this.dgvDSNguyenLieu.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvDSNguyenLieu.RowTemplate.Height = 30;
+            this.dgvDSNguyenLieu.RowTemplate.ReadOnly = true;
+            this.dgvDSNguyenLieu.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDSNguyenLieu.Size = new System.Drawing.Size(920, 481);
+            this.dgvDSNguyenLieu.TabIndex = 2;
+            this.dgvDSNguyenLieu.TabStop = false;
+            this.dgvDSNguyenLieu.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDSNguyenLieu_CellClick);
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 50F;
+            this.Column2.HeaderText = "Mã nguyên liệu";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Tên nguyên liệu";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 70F;
+            this.Column4.HeaderText = "Số lượng còn lại";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Column5
+            // 
+            this.Column5.FillWeight = 50F;
+            this.Column5.HeaderText = "Đơn vị tính";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(920, 44);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "DANH SÁCH NGUYÊN LIỆU";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label13.Location = new System.Drawing.Point(12, 79);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(100, 26);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "Tìm kiếm";
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiem.ForeColor = System.Drawing.Color.Peru;
+            this.txtTimKiem.Location = new System.Drawing.Point(118, 77);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(347, 30);
+            this.txtTimKiem.TabIndex = 19;
+            this.txtTimKiem.TabStop = false;
+            this.txtTimKiem.TextChanged += new System.EventHandler(this.txtTimKiem_TextChanged);
+            // 
+            // lblMaNL
+            // 
+            this.lblMaNL.BackColor = System.Drawing.Color.Transparent;
+            this.lblMaNL.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaNL.ForeColor = System.Drawing.Color.Peru;
+            this.lblMaNL.Location = new System.Drawing.Point(406, 603);
+            this.lblMaNL.Name = "lblMaNL";
+            this.lblMaNL.Size = new System.Drawing.Size(526, 26);
+            this.lblMaNL.TabIndex = 29;
+            this.lblMaNL.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(7, 603);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 26);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Mã nguyên liệu:";
+            // 
+            // lblTenNL
+            // 
+            this.lblTenNL.BackColor = System.Drawing.Color.Transparent;
+            this.lblTenNL.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenNL.ForeColor = System.Drawing.Color.Peru;
+            this.lblTenNL.Location = new System.Drawing.Point(406, 638);
+            this.lblTenNL.Name = "lblTenNL";
+            this.lblTenNL.Size = new System.Drawing.Size(526, 26);
+            this.lblTenNL.TabIndex = 31;
+            this.lblTenNL.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label3.Location = new System.Drawing.Point(7, 638);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 26);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Tên nguyên liệu: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(205, 664);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 32);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "NHẬP SỐ LƯỢNG LẤY";
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Enabled = false;
+            this.txtSoLuong.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoLuong.ForeColor = System.Drawing.Color.Peru;
+            this.txtSoLuong.Location = new System.Drawing.Point(211, 699);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(559, 30);
+            this.txtSoLuong.TabIndex = 33;
+            this.txtSoLuong.TabStop = false;
+            this.txtSoLuong.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSoLuong_KeyDown);
+            this.txtSoLuong.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoLuong_KeyPress);
+            // 
+            // lblDonViTinh
+            // 
+            this.lblDonViTinh.BackColor = System.Drawing.Color.Transparent;
+            this.lblDonViTinh.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDonViTinh.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lblDonViTinh.Location = new System.Drawing.Point(776, 701);
+            this.lblDonViTinh.Name = "lblDonViTinh";
+            this.lblDonViTinh.Size = new System.Drawing.Size(76, 26);
+            this.lblDonViTinh.TabIndex = 34;
+            this.lblDonViTinh.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnXacNhan
+            // 
+            this.btnXacNhan.Appearance.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXacNhan.Appearance.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnXacNhan.Appearance.Options.UseFont = true;
+            this.btnXacNhan.Appearance.Options.UseForeColor = true;
+            this.btnXacNhan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnXacNhan.Enabled = false;
+            this.btnXacNhan.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnXacNhan.ImageOptions.Image")));
+            this.btnXacNhan.Location = new System.Drawing.Point(287, 735);
+            this.btnXacNhan.Name = "btnXacNhan";
+            this.btnXacNhan.Size = new System.Drawing.Size(413, 53);
+            this.btnXacNhan.TabIndex = 35;
+            this.btnXacNhan.Text = "Xác nhận";
+            this.btnXacNhan.Click += new System.EventHandler(this.btnXacNhan_Click);
+            // 
+            // frmLayNguyenLieu
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.Linen;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 802);
+            this.Controls.Add(this.btnXacNhan);
+            this.Controls.Add(this.lblDonViTinh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSoLuong);
+            this.Controls.Add(this.lblTenNL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMaNL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dgvDSNguyenLieu);
+            this.IconOptions.Image = global::BakeryManagementSystem.Properties.Resources.icons8_take_32;
+            this.MaximizeBox = false;
+            this.Name = "frmLayNguyenLieu";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lấy nguyên liệu";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmLayNguyenLieu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSNguyenLieu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDSNguyenLieu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Label lblMaNL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTenNL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.Windows.Forms.Label lblDonViTinh;
+        private DevExpress.XtraEditors.SimpleButton btnXacNhan;
+    }
+}
